68b404abdb1834b52e2e1dcc263fc198642778a2
Lamda <takatim1574@icloud.com>
2018-03-14 19:19:43 +0900

    Add files via upload

---
+++ src/jp/mazaicrafty/pmmp/CheckPing/main.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * ___  ___               _ _____            __ _         
+ * |  \/  |              (_)  __ \          / _| |        
+ * | .  . | __ _ ______ _ _| /  \/_ __ __ _| |_| |_ _   _ 
+ * | |\/| |/ _` |_  / _` | | |   | '__/ _` |  _| __| | | |
+ * | |  | | (_| |/ / (_| | | \__/\ | | (_| | | | |_| |_| |
+ * \_|  |_/\__,_/___\__,_|_|\____/_|  \__,_|_|  \__|\__, |
+ *                                                   __/ |
+ *                                                  |___/
+ * Copyright (C) 2017-2018 @MazaiCrafty (https://twitter.com/MazaiCrafty)
+ *
+ * This program is free plugin.
+ */
+
+namespace jp\mazaicrafty\pmmp\CheckPing;
+
+# Base
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\Player;
+
+# Commands
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\CommandExecuter;
+use pocketmine\command\ConsoleCommandSender;
+
+# Utils
+use pocketmine\utils\TextFormat as COLOR;
+use pocketmine\utils\Config;
+
+class main extends PluginBase{
+
+    const PREFIX = "§a[§bCheckPing§a]§r ";
+    const VERSION = "1.0.0";
+
+    public function onEnable(): void{
+        $this->allRegisterEvents();
+        $this->enableMessage();
+    }
+
+    public function enableMessage(): void{
+        Server::getInstance()->getLogger()->info(self::PREFIX . COLOR::YELLOW . "is Enabling!");
+        Server::getInstance()->getLogger()->info(self::PREFIX . COLOR::AQUA . "Version " . COLOR::GREEN . self::VERSION);
+        Server::getInstance()->getLogger()->info(self::PREFIX . COLOR::GRAY . "https://github.com/MazaiCrafty");
+        Server::getInstance()->getLogger()->info(self::PREFIX . COLOR::WHITE . "Thank you for observing the specified license." . COLOR::BLUE . " by @MazaiCrafty");
+    }
+    
+    public function allRegisterEvents(): void{
+        if(!file_exists($this->getDataFolder())){
+            mkdir($this->getDataFolder(), 0755, true);
+        }
+        $dir = $this->getDataFolder();
+        $this->config = new Config($dir . "Config.yml", Config::YAML, array(
+            "message" => "§aPong %PING%ms"
+        ));
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool{
+        switch ($cmd->getName()){
+            case "ping":
+            $ping = $sender->getPing();
+            $str = $this->config->get("message");
+            $message = str_replace("%PING%", $ping, $str);
+            $sender->sendMessage($message);
+            return true;
+        }
+        return true;
+    }
+}
